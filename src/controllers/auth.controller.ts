7d14b7d132a5aecd7bc448889ca5a10012b87c23
Abdi-01 <abdialghi@gmail.com>
2025-06-23 20:54:36 +0700

    sign in

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { prisma } from "../config/prisma";
 import { hashPassword } from "../utils/hash";
+import { compare } from "bcrypt";
 
 // REGISTER USER
 export const registerUser = async (
@@ -43,6 +44,28 @@ export const loginUser = async (
   next: NextFunction
 ) => {
   try {
+    const findUser = await prisma.user.findUnique({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!findUser) {
+      throw { rc: 404, message: "User not exist" };
+    }
+
+    const comparePass = await compare(req.body.password, findUser.password);
+    if (!comparePass) {
+      throw { rc: 401, message: "Password is wrong" };
+    }
+
+    res
+      .status(200)
+      .send({
+        username: findUser.username,
+        email: findUser.email,
+        imgProfile: findUser.imgProfile,
+      });
   } catch (error) {
     next(error);
   }
